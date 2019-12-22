181f345214204063a4903df35aa645abba1681e6
Lindsay M <lindsaymecher@gmail.com>
2019-12-22 02:21:49 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages][0]
+     dennis_ritchies_language
 end
 
 def adding_matz
@@ -79,7 +85,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] ={}
+programmer_hash
     
 end
 
@@ -101,7 +108,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
      
 end
 
@@ -122,4 +131,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
